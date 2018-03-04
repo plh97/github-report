7c7cebe37d1ec6c887fe79632bc4e5881adc6d57
pengliheng <pengliheng111@gmail.com>
2018-03-04 16:19:31 +0800

    123

---
+++ example/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ReactGithubCalendar from '../../dist/index.js';
+
+render(
+	<ReactGithubCalendar />,
+	document.getElementById('root')
+);
