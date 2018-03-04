576c79a3aab9b61730c4d5d47d92c1263014751e
pengliheng <pengliheng111@gmail.com>
2018-03-04 13:25:56 +0800

    init github-report

---
+++ github/index.jsx
@@ -0,0 +1,18 @@
+// package
+import React, { Component } from 'react';
+
+// local
+import './index.less';
+import Github from './Github.jsx';
+
+class GithubReport extends Component {
+  render() {
+    return (
+      <div>
+        <Github />
+      </div>
+    );
+  }
+}
+
+export default GithubReport;
