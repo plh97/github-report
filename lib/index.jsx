6ee3077bad1cf47ed6c19830c33587bd2badb298
pengliheng <pengliheng111@gmail.com>
2018-05-24 02:31:46 +0800

    2r3

--- lib/index.jsx
+++ lib/index.jsx
@@ -65,68 +65,68 @@ export default class Github extends Component {
     }, 0);
   }
 
-  getData({ name }) {
-    return new Promise((resolve, reject) => {
-      axios({
-        url: '/graphql',
-        method: 'post',
-        data: {
-          query: `{
-            search(query: "${name || 'pengliheng'}", type: USER, first: 1) {    
-              edges {
-                node {
-                  ... on User {
-                    avatarUrl login bio url createdAt
-                    contributedRepositories(first: 100,orderBy: {field: CREATED_AT, direction: DESC}) {
-                      totalCount
-                      nodes{
-                        nameWithOwner url
-                      }
-                    }
-                    starredRepositories(first:100) {
-                      nodes {
-                        primaryLanguage {
-                          name color
-                        }
-                      }
-                    }
-                    followers(first: 100) {
-                      totalCount
-                      nodes {
-                        url name avatarUrl login
-                      }
-                    }
-                    following(first: 100) {
-                      totalCount
-                      nodes {
-                        url name avatarUrl login
-                      }
-                    }
-                    repositories(first:100,orderBy: {field: STARGAZERS, direction: DESC}){
-                      totalCount
-                      nodes{
-                        createdAt updatedAt isFork name url
-                        primaryLanguage {
-                          name
-                        }
-                        forks(first:0){
-                          totalCount
-                        }
-                        stargazers(first:0){
-                          totalCount
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }`,
-        },
-      }).then(res => resolve(res.data.data))
-        .catch(err => reject(err));
-    });
-  }
+  // getData({ name }) {
+  //   return new Promise((resolve, reject) => {
+  //     axios({
+  //       url: '/graphql',
+  //       method: 'post',
+  //       data: {
+  //         query: `{
+  //           search(query: "${name || 'pengliheng'}", type: USER, first: 1) {    
+  //             edges {
+  //               node {
+  //                 ... on User {
+  //                   avatarUrl login bio url createdAt
+  //                   contributedRepositories(first: 100,orderBy: {field: CREATED_AT, direction: DESC}) {
+  //                     totalCount
+  //                     nodes{
+  //                       nameWithOwner url
+  //                     }
+  //                   }
+  //                   starredRepositories(first:100) {
+  //                     nodes {
+  //                       primaryLanguage {
+  //                         name color
+  //                       }
+  //                     }
+  //                   }
+  //                   followers(first: 100) {
+  //                     totalCount
+  //                     nodes {
+  //                       url name avatarUrl login
+  //                     }
+  //                   }
+  //                   following(first: 100) {
+  //                     totalCount
+  //                     nodes {
+  //                       url name avatarUrl login
+  //                     }
+  //                   }
+  //                   repositories(first:100,orderBy: {field: STARGAZERS, direction: DESC}){
+  //                     totalCount
+  //                     nodes{
+  //                       createdAt updatedAt isFork name url
+  //                       primaryLanguage {
+  //                         name
+  //                       }
+  //                       forks(first:0){
+  //                         totalCount
+  //                       }
+  //                       stargazers(first:0){
+  //                         totalCount
+  //                       }
+  //                     }
+  //                   }
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }`,
+  //       },
+  //     }).then(res => resolve(res.data.data))
+  //       .catch(err => reject(err));
+  //   });
+  // }
   initGitRepo(res) {
     const { langColor } = this.state;
     const viewer = res.search.edges[0].node;
